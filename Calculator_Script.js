43c954f49cb1f9d62340e530131e3bfaa9c8f911
Tommy Keirnan <tkeirnan@gmail.com>
2020-01-31 23:04:39 -0500

    improved interface. started working on javascript

---
+++ Calculator_Script.js
@@ -0,0 +1,44 @@
+// create interface - display, number keys, decimal key, clearEverything key, equals key, operator keys.
+
+//display should be set to 0.
+//ClearEverythingKey should be set to CE.
+
+// First Input: A. If NumberKey - replace 0 on display with number.
+//              B. If DecimalKey - change CE key to C. and place decimal after 0.
+//              C. If ClearEverythingKey - do nothing.
+//              D. If EqualsKey - do nothing.
+//              E. If MultiplicationKey, AdditionKey, SubtractionKey - change color of key.
+//              F. If DivisionKey - do nothing.
+
+// Second Input: If A plus A - render string of As on display.
+//               If A plus B - place decimal after A.
+//               If A plus C - clear display and revert to 0.
+//               If A plus D - do nothing.
+//               If A plus E - do nothing to display - change color of key.
+//               If A plus F - do nothing to display - do nothing to key.
+//               If B plus A - place number key after "0." in display.
+//               if B plus B - do nothing.
+
+// Third Input:
+
+
+
+//add an event listener that turns the number on the display to zero.
+
+//add an event listener that adds an operator to the number of the display 
+
+// Add if else function for third event. If number add it to equation, Else, replace previous operator.
+
+//add an event listener to EqualsKey that evaluates first, second, and third input.
+
+
+//and replaces any previous operator if any.
+
+// if a number key is pressed after a number and an operator, add it to the equation
+
+// 
+
+//add an event listener that adds a number the value of the diplay, the operator, and a third value, 
+//and renders the result in the display.  
+
+// create event listener that clears any number or operator on the display.
